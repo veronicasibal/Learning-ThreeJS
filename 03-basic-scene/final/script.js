8ab8910df8bbed4a01c679bed639c9665b6daaf8
veronicasibal <veronica.k.sibal@icloud.com>
2026-03-24 14:19:54 -0500

    Adding lesson 03 - basic scene

---
+++ 03-basic-scene/final/script.js
@@ -0,0 +1,34 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+//classes inside of THREE library
+
+//Scene
+const scene = new THREE.Scene()
+
+//Object
+const geometry = new THREE.BoxGeometry(1, 1, 1) //width, height, depth
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }) 
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+//Camera 
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) 
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer 
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera) 
